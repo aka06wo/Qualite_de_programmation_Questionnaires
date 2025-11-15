c657ab9fa09d60a585fc54f495ec7ec81bff8549
Msd675 <mscadi675@gmail.com>
2025-11-15 23:03:46 +0100

    Verification du code et erreur possibles

--- QuestionChoixMultiple.cpp
+++ QuestionChoixMultiple.cpp
@@ -1,39 +1,27 @@
 #include "QuestionChoixMultiple.h"
 
-QuestionChoixMultiple::QuestionChoixMultiple(const string &question, const std::vector<string> &reponse) :
-    Question{question}, d_reponse{reponse}
+QuestionChoixMultiple::QuestionChoixMultiple(const string &question, 
+                    const std::vector<string> &reponsesPossibles, int numReponse) :
+    Question{question}, d_reponsesPossibles{reponsesPossibles}, d_numReponse{numReponse}
 {
 }
 
 string QuestionChoixMultiple::reponse() const {
-    string rep="Les différentes options sont entres autres :\n" ;
-    for (const auto &r : d_reponse) {
-        rep += r + " ";
-    }
-    return rep ;
-}
-std::vector<string> QuestionChoixMultiple::reponseQM() const {
-    return d_reponse;
+    return d_reponsesPossibles[d_numReponse];
 }
 
 void QuestionChoixMultiple::afficherReponse() const {
     std::cout<<reponse()<<std::endl;
 }
 
-bool QuestionChoixMultiple::verificationReponse(string reponse) const {
-    return true ;
-}
+// revoir la modélisation bool QuestionChoixMultiple::verificationReponse(STRING reponse) const
 bool QuestionChoixMultiple::verificationReponse(int reponse) const {
-    int i=0 ;
-    while (i<d_reponse.size() && i+1!=reponse) {
-        i++ ;
-    }
-
-    return i!=d_reponse.size() ;
+    return reponse==d_numReponse ;
 }
 
 std::unique_ptr<Question> QuestionChoixMultiple::clone() const {
     // on fait un std::move ou pas ?? (je ne sais pas encore, j'ai oublié)
+    // utilisé dans le constructeur de questionnaires
     // ??
-    return std::make_unique<QuestionChoixMultiple>(intitule(),reponseQM()) ;
+    return std::make_unique<QuestionChoixMultiple>(intitule(),d_reponsesPossibles,d_numReponse) ;
 }
