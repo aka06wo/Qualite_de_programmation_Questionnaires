55543de6016446b0d103182c3d2a177742df9d94
SidyDCode <sidysn.diop@gmail.com>
2025-11-16 20:15:06 +0100

    déclaration et définition de certaines méthodes de oe classe

--- Question.h
+++ Question.h
@@ -17,7 +17,7 @@ public :
     virtual bool poserQuestion() const = 0 ; // on lui pose une question et on verifie s'il elle est juste ou pas
     virtual void afficherQuestion() const ;
     virtual void afficherReponse() const = 0;
-    virtual string Reponse() const = 0;
+    virtual string reponse() const = 0;
     virtual void sauvegarder(ostream& os) const = 0; // pour que chaque question sache se sauvegarder elle meme
     virtual void charger(istream& ist) = 0;
     virtual string Type_Question() const = 0; //savoir le type de question: si c'est numerique , texte ,ou choix multiple
